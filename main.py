9c10e076d8d5ef71122be182578a6ada06be9545
Julien Dcruz <juliendcruz@gmail.com>
2016-12-07 20:10:52 +0530

    create initial ui

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Application entry module
+author: Julien Dcruz
+website: zetcode.com
+last edited: 7th December 2016
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from maindwindow import MainWindow
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.setWindowTitle('Imagus')
+    w.showMaximized()
+
+    sys.exit(app.exec_())
---
+++ maindwindow.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow
+from ui import ui_mainwindow
+
+
+class MainWindow(QMainWindow, ui_mainwindow.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
