89221fb215652d047b6e438e752a9d92f8482dd9
Julien Dcruz <juliendcruz@gmail.com>
2017-10-10 19:58:30 +0530

    Use QGraphicsView for rendering thumbs

--- src/qpicasa/main_window.py
+++ src/qpicasa/main_window.py
@@ -10,6 +10,7 @@ from PyQt5.QtCore import pyqtSignal, pyqtSlot, QSize, QThread, QModelIndex
 from PyQt5.QtGui import QStandardItemModel, QStandardItem, QFont
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMainWindow, QFileDialog, QGridLayout, QLabel
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsPixmapItem, QGraphicsDropShadowEffect
 from .folder_manager import FolderManager
 from .meta_files import MetaFilesManager
 from .scan_dir_loader import ScanDirLoader
@@ -25,6 +26,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def __init__(self, parent=None):
         self.w = None
+        self._img_serial_no = 0
         self._scan_dir_loader_thread = QThread()
         self.folder_mgr = FolderManager()
         self._meta_files_mgr = MetaFilesManager()
@@ -43,16 +45,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self._scan_dir_loader_thread.start()
         LOGGER.info('Image loader thread started.')
 
+        #Setup the thumbs gfx scene
+        self._thumbs_gfx_scene = QGraphicsScene()
+        self._thumbs_gfx_scene.addText("JULIEN")
+        self.gfxview_thumbs.setScene(self._thumbs_gfx_scene)
+        self.gfxview_thumbs.setAlignment(QtCore.Qt.AlignTop | QtCore.Qt.AlignLeft)
+
         #Begin loading the currently selected dir
         if self.listView_scandirs.model():
             selIndex = self._dirs_list_model.index(0,0)
             self.listView_scandirs.setCurrentIndex(selIndex)
             selected = self.listView_scandirs.selectedIndexes()
             if len(selected) > 0:
-                self._load_dir_images(selected[0])
+                 self._load_dir_images(selected[0])
         
-        self._thumbs_layout = QGridLayout()
-        self.scrollArea.setLayout(self._thumbs_layout)
 
     def _setup_connections(self):
         self.action_FolderManager.triggered.connect(self.action_FolderManager_Clicked)
@@ -84,17 +90,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         sd_id = model_index.data(QtCore.Qt.UserRole + 1)
         self._dir_load_start.emit(sd_id)
 
-    def _clear_thumbs_layout(self):
-        for i in reversed(range(self._thumbs_layout.count())):
-            self._thumbs_layout.takeAt(i).widget().setParent(None)
+    def _clear_thumbs(self):
+        self._thumbs_gfx_scene.clear()
+        self._img_serial_no = 0
     
     @pyqtSlot(object)
     def on_dir_images_load_success(self, img):
         LOGGER.debug('Image loaded.' + img["name"])
-        label = QLabel()
-        label.setPixmap(QPixmap.fromImage(img['thumb']))
-        label.setMaximumSize(QSize(128, 128))
-        self._thumbs_layout.addWidget(label)
+
+        item = QGraphicsPixmapItem()
+        item.setPixmap(QPixmap.fromImage(img['thumb']))
+
+        thumb_shadow_effect = QGraphicsDropShadowEffect()
+        thumb_shadow_effect.setOffset(3.0)
+        item.setGraphicsEffect(thumb_shadow_effect)
+
+        item.setPos(self._img_serial_no * 150, 0)
+        self._thumbs_gfx_scene.addItem(item)
+        self._img_serial_no = self._img_serial_no + 1
+
 
     @pyqtSlot()
     def on_dir_images_load_ended(self):
@@ -104,7 +118,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     @pyqtSlot(QModelIndex)
     def on_scan_dir_listview_clicked(self, index):
-        self._clear_thumbs_layout()
+        self._clear_thumbs()
         self._load_dir_images(index)
         
 
