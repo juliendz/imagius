fb61ba30c431ee6e99f8b0ac1e354a94c76945d5
Julien Dcruz <juliendcruz@gmail.com>
2018-03-17 12:16:24 +0530

    Add Image Properties Window

--- src/qpicasa/ui/ui_propertieswidget.py
+++ src/qpicasa/ui/ui_propertieswidget.py
@@ -11,20 +11,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_PropertiesWidget(object):
     def setupUi(self, PropertiesWidget):
         PropertiesWidget.setObjectName("PropertiesWidget")
-        PropertiesWidget.resize(418, 283)
+        PropertiesWidget.resize(270, 465)
         self.verticalLayout = QtWidgets.QVBoxLayout(PropertiesWidget)
         self.verticalLayout.setObjectName("verticalLayout")
         self.gbox_properties = QtWidgets.QGroupBox(PropertiesWidget)
         font = QtGui.QFont()
         font.setFamily("Noto Sans")
-        font.setPointSize(14)
+        font.setPointSize(10)
         self.gbox_properties.setFont(font)
         self.gbox_properties.setObjectName("gbox_properties")
-        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.gbox_properties)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.listWidget_properties = QtWidgets.QListWidget(self.gbox_properties)
-        self.listWidget_properties.setObjectName("listWidget_properties")
-        self.verticalLayout_2.addWidget(self.listWidget_properties)
         self.verticalLayout.addWidget(self.gbox_properties)
 
         self.retranslateUi(PropertiesWidget)
