9adae52de1516165c4980ad84a8852bb5ebbe54a
Julien Dcruz <juliendcruz@gmail.com>
2018-03-24 10:08:04 +0530

    Add fail check for github release api

--- src/imagius/update_manager.py
+++ src/imagius/update_manager.py
@@ -2,7 +2,7 @@
 Update Manager module
 author: Julien Dcruz
 """
-from PyQt5 import QtCore, QtNetwork
+from PyQt5 import QtCore, QtNetwork, QtGui, QtWidgets
 from packaging.version import Version
 
 from imagius_types import SettingType
@@ -21,7 +21,8 @@ class UpdateManager(QtCore.QObject):
 
     def get_updates(self):
         # url = settings.get(SettingType.CHECK_UPDATE_URL, 'https://api.github.com/repos/juliendz/imagius/releases')
-        url = 'https://api.github.com/repos/reactiveui/ReactiveUI/releases/latest'
+        # url = 'https://api.github.com/repos/reactiveui/ReactiveUI/releases/latest'
+        url = 'https://api.github.com/repos/reactiveui/ReactiveiUI/releases/latest'
         qurl = QtCore.QUrl(url)
         LOGGER.info("Checking for updates....")
         self._net_mgr.get(QtNetwork.QNetworkRequest(qurl))
@@ -33,10 +34,13 @@ class UpdateManager(QtCore.QObject):
             json_obj = json_doc.object()
             latest_release = Version(json_obj['tag_name'].toString())
             if latest_release > settings.app_version:
-                LOGGER.info("Update found....")
+                LOGGER.info("Update found. New version(%s) is available" % latest_release)
             else:
-                LOGGER.info("No updates found....")
-        else:
-            LOGGER.error("Error while making update check request!")
-        # elif reply.error() == QtNetwork.QNetworkReply.HostNotFoundError:
-
+                LOGGER.info("No updates found! Already on latest release.")
+        else: 
+            LOGGER.error("Update Manager: Unable to retrieve updates from release api (Error: %s)" % reply.error())
+            msgbox = QtWidgets.QMessageBox()
+            msgbox.setText('Unable to load the release api. Redirecting to website downloads page.')
+            msgbox.setDetailedText('Network Request Error Code: %s' % reply.error())
+            msgbox.exec()
+            QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://juliendz.github.io/imagius/#download"))
