aa7cafe2da147e6f9028416542fe3417e4d914a2
Julien Dcruz <juliendcruz@gmail.com>
2019-06-26 17:10:19 +0530

    Added qt c++ project

---
+++ imagius/src/image.h
@@ -0,0 +1,17 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <QObject>
+
+class Image : public QQuickItem
+{
+        Q_OBJECT
+    public:
+        Image();
+
+    signals:
+
+    public slots:
+};
+
+#endif // IMAGE_H
